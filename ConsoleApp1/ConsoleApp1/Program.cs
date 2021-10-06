65741cee4389c97a153740473ebc8e0877ebc0cb
Mariia0Shcherbak <masha.shcherbak.2016@gmail.com>
2021-10-06 22:44:10 +0300

    DZ1 06.10.2021

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,12 +6,45 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Program
+    class Programz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GIT!!!");
-            Console.WriteLine("Hello GIT!!!");
+         
+            Console.Write("Введите а=> _\b");
+            string str = Console.ReadLine();
+            double a= Convert.ToDouble(str);
+            Console.Write("Введите b=> _\b");
+            str = Console.ReadLine();
+            double b = Convert.ToDouble(str);
+            Console.Write("Введите c=> _\b");
+            str = Console.ReadLine();
+            double c = Convert.ToDouble(str);
+            Console.WriteLine("--------------------");
+            double dis = b * b - 4 * a * c;
+            if (dis < 0)
+            {
+                Console.WriteLine("Нет действительных корней");
+
+            }
+            else
+            {
+                double x = (-b - Math.Sqrt(dis)) / (2 * a);
+                double x1 = (-b + Math.Sqrt(dis)) / (2 * a);
+                if (x == x1)
+                {
+                    Console.WriteLine("x={0}", x);
+                }
+                else
+                {
+                    Console.WriteLine("x={0}", x);
+                    Console.WriteLine("x={0}", x1);
+                }
+            }
+         
+            
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
         }
     }
 }
